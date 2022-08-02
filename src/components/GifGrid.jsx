008e8c4417b91097fcf396c6342cff723676eb21
gustavocontin1 <contin.gustavo1@gmail.com>
2022-08-02 18:51:58 -0300

    actualizacion commit

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -4,7 +4,6 @@ import useFetchGifs from "../hooks/useFetchGifs"
 const GifGrid = ({category}) => {
     const {images, isLoading} = useFetchGifs(category)
 
-    
     return (
         <>
             <h3>{category}</h3>
